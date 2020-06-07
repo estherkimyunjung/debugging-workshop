b4884c6b6a7391d7e69392d1aa127f5fdbd0d1cc
estherkimyunjung <estherkimyunjung@gmail.com>
2020-06-07 16:38:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
+    
   const form = document.getElementById('joke-form')
-  const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
-  let joke;
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => jokeData.joke)
   }
 
   form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const jokeList = document.getElementById('joke-list')
+    const newJokeLi = document.createElement('li')
+    const username = document.getElementById('name-input').value
 
     if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+
+    fetchJoke().then(joke => {
+
+      newJokeLi.innerHTML = `
+      <span class="username">${username} says:</span> ${joke}
+      `
+
+      jokeList.appendChild(newJokeLi)
+    })
   })
-})
+    
+  // <div id="main-container">
+  //   <div>
+  //     <form id="joke-form">
+  //       <h2>Make an awesome joke</h2>
+  //       <p>Your Name: <input type="text" id="name-input" placeholder="Your Name" autocomplete="off"></input></p>
+  //       <p><input type="submit" value="Click here to make a joke"></input></p>
+  //     </form>
+  //   </div>
+  //   <ul id="joke-list">
+  //   </ul>
+  // </div>
+  
+  // })
